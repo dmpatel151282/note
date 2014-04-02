e0531c28d0318d270ba3529cc36b0089ef04a51c
liushui <15980936990@163.com>
2014-04-02 18:13:18 +0800

    asdf

--- linux/driver/input/input.c
+++ linux/driver/input/input.c
@@ -1,84 +1,97 @@
-Input子系统
 http://blog.csdn.net/ielife/article/details/7798952
+
 linux输入子系统
     1. 输入子系统事件处理层（EventHandler）
+
+        gsensor 周期性汇报即可  -> 轮询  -> input-polldev.c
+        TP      需立即响应      -> 中断
 
     2. 输入子系统核心层（InputCore）
 
     3. 输入子系统设备驱动层
       主要实现对硬件设备的读写访问，中断设置，并把硬件产生的事件转换为核心层定义的规范提交给事件处理层。
 
+include/linux/input.h
+重要结构体
+    1）struct input_event
+         time:  struct timeval
+         type:  __u16 
+         code:  __u16 
+         value: __s32
+
+    2）struct input_dev      // input device
+
+        node: struct list_head   // input_dev list 
 
 
+    3）struct input_handler  // interfaces of input devices
+        id_table:  struct input_device_id
+        node:      struct list_head     //handler list, eg. evdev_handler, mousedev_handler
+        h_node:    struct list_head
+       //回调函数
+        *connect            //called when attaching a handler to an input device
+        *disconnect
+        *filter
+        *event
+        *start
 
+    4）struct input_handle  // links input device with an input handler
+        input_dev:
+        input_handler:
 
+        d_node: struct list_head
+        h_node: struct list_head
 
+----------------------------------------------------------------------------------------
+input-core 为外部提供的核心函数 (input.c)
 
+    EXPORT_SYMBOL(input_allocate_device); // 使用宏 input_add_device
+    EXPORT_SYMBOL(input_free_device);
 
+    EXPORT_SYMBOL(input_register_device);
+    EXPORT_SYMBOL(input_unregister_device);
 
+    EXPORT_SYMBOL(input_register_handler);
+    EXPORT_SYMBOL(input_unregister_handler);
 
+    EXPORT_SYMBOL(input_handler_for_each_handle);
 
+    EXPORT_SYMBOL(input_event);         //报告事件，使用以下宏函数
+      void input_report_key(struct input_dev *dev, unsigned int code, int value);
+      void input_report_abs(struct input_dev *dev, unsigned int code, int value)
+      void input_report_rel(struct input_dev *dev, unsigned int code, int value)
+      void input_sync(struct input_dev *dev)
 
+    EXPORT_SYMBOL(input_set_abs_params);
 
+----------------------------------------------------------------------------------------
+input-polldev.h    轮询方式报告事件
+input-polldev.c
 
+struct input_polled_dev {
+    void *private;
 
-1. 数据结构
-    input_dev   
-    input_event 事件格式
+    void (*open)(struct input_polled_dev *dev);
+    void (*close)(struct input_polled_dev *dev);
+    void (*poll)(struct input_polled_dev *dev);
+    unsigned int poll_interval;     /* msec */
+    unsigned int poll_interval_max; /* msec */
+    unsigned int poll_interval_min; /* msec */
 
-struct input_dev {
-	const char *name;//设备名
-	const char *phys;
-	const char *uniq;
-	struct input_id id;
+    struct input_dev *input;
 
-    //设备支持的事件类型，如EV_KEY,EV_REL 
-	unsigned long evbit[BITS_TO_LONGS(EV_CNT)];
-	unsigned long keybit[BITS_TO_LONGS(KEY_CNT)];//如KEY_A, KEY_UP
-	unsigned long relbit[BITS_TO_LONGS(REL_CNT)];
-	unsigned long absbit[BITS_TO_LONGS(ABS_CNT)];//具体事件，如ABS_X,ABS_Y
-	unsigned long mscbit[BITS_TO_LONGS(MSC_CNT)];
-	unsigned long ledbit[BITS_TO_LONGS(LED_CNT)];
-	unsigned long sndbit[BITS_TO_LONGS(SND_CNT)];
-	unsigned long ffbit[BITS_TO_LONGS(FF_CNT)];
-	unsigned long swbit[BITS_TO_LONGS(SW_CNT)];
-
-	struct timer_list timer;
-    ....
+    /* private: */
+    struct delayed_work work;
 };
 
-struct input_event {
-	struct timeval time;//事件时间戳
-	__u16 type;         //事件类型
-	__u16 code;         //事件编码
-	__s32 value;        //事件值
-};
+struct input_polled_dev *input_allocate_polled_device(void);
+void input_free_polled_device(struct input_polled_dev *dev);
+int input_register_polled_device(struct input_polled_dev *dev);
+void input_unregister_polled_device(struct input_polled_dev *dev);
 
-
-2. 函数接口
-struct input_dev *input_allocate_device(void);
-void input_free_device(struct input_dev *dev);
-
-int input_register_device(struct input_dev *dev);
-void input_unregister_device(struct input_dev *dev);
-
-/* 时钟使能 */
-struct clk *clk_get(struct device *dev, const char *id);
-int clk_enable(struct clk *clk);
-void clk_disable(struct clk *clk);
-void clk_put(struct clk *clk);
-
-/*  code, 事件编码; value, 事件对应值, 0 松开, 1 按下 */
-void input_report_key(struct input_dev *dev, unsigned int code, int value);
-void input_report_abs(struct input_dev *dev, unsigned int code, int value)
-void input_report_rel(struct input_dev *dev, unsigned int code, int value)
-void input_sync(struct input_dev *dev)
-
-void input_set_abs_params(struct input_dev *dev, int axis, int min, int max, int fuzz, int flat);
-
-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 调试
-    getevent sendevent 是android系统下的一个工具，可以模拟多种按键和触屏操作i
+    getevent sendevent 是android系统下的一个工具，可以模拟多种按键和触屏操作
   产生的是raw event，raw event经过event hub处理产生最终的gesture事件。
   
     源码：system/core/toolbox/ sendevent.c getevent.c
